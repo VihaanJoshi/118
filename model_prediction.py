29d13234af921ed42e1d3b1b521e48b3a1cb7302
VihaanJoshi <77791582+VihaanJoshi@users.noreply.github.com>
2023-05-19 14:00:42 +0530

    Add files via upload

---
+++ model_prediction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from datetime import datetime
